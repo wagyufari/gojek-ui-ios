20293e8c2b3a636fd60ee8d560ae73ba141f5cf0
Muhammad Ghifari <ghifari9567@gmail.com>
2021-07-17 22:05:28 +0700

    Initial Commit

---
+++ Gojek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gojek
+//
+//  Created by Muhammad Ghifari on 17/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
